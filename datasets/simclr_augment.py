8997c80ac0041b05a79d7090ee0fc71bc6bf3ea0
Shirohati <zxr188721839@163.com>
2026-03-17 22:58:22 +0800

    docs：更新注释

--- datasets/simclr_augment.py
+++ datasets/simclr_augment.py
@@ -11,8 +11,9 @@ class SimCLRTransform:
             T.RandomHorizontalFlip(p=0.5),
             T.ColorJitter(0.4, 0.4, 0.4, 0.1),  # 颜色抖动
             T.RandomGrayscale(p=0.2),
-            T.ToTensor(),
-            T.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])  # CIFAR-10 统计数据
+            T.ToTensor(), #转化为(通道,长,宽)的格式并将像素值转移到(0,1)之中，常常用在数据增强之后，这是使用pytorch的前提条件
+            T.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2023, 0.1994, 0.2010])  #标准化，参数要根据数据集的均值设定
+            # CIFAR-10 统计数据
         ])
 
     def __call__(self, x):
